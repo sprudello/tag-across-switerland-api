5aae1e2d42e695e563ef51d2c006321018732038
sprudello <110892985+sprudello@users.noreply.github.com>
2024-02-22 16:01:52 +0100

    Group added

---
+++ TagAPI/Models/Group.cs
@@ -0,0 +1,14 @@
+﻿namespace TagAPI.Models
+{
+    public class Group
+    {
+        public int ID { get; set; }
+        public required string GroupName { get; set; } = string.Empty;
+        public string PasswordHash { get; set; }
+        public int GottstattCoins { get; set; }
+    }
+    public class GroupChallenge
+    {
+
+    }
+}
