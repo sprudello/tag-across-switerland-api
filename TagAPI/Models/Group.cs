ec293641d58681d0769fcd6db774b06d15eb0f9f
sprudello <110892985+sprudello@users.noreply.github.com>
2024-03-01 11:40:53 +0100

    Added some table classes

--- TagAPI/Models/Group.cs
+++ TagAPI/Models/Group.cs
@@ -4,11 +4,15 @@
     {
         public int ID { get; set; }
         public required string GroupName { get; set; } = string.Empty;
-        public string PasswordHash { get; set; }
+        public string PasswordHash { get; set; } = string.Empty;
         public int GottstattCoins { get; set; }
     }
-    public class GroupChallenge
+    class ChallengeCard
     {
+        public int CardID { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int Reward { get; set; }
 
     }
 }
---
+++ TagAPI/Models/GroupChallenge.cs
@@ -0,0 +1,12 @@
+﻿namespace TagAPI.Models
+{
+    public class GroupChallenge
+    {
+        public int GroupChallengeID { get; set; }
+        public int GroupID { get; set; }
+        public int CardID { get; set; }
+        public required DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Status { get; set; } = string.Empty;
+    }
+}
